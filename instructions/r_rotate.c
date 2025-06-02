72bfeaa25389b82fe076e64f36335462500c9ef6
Alois <amarti@student.42lehavre.fr>
2025-06-02 09:03:15 +0200

    norminette

--- instructions/r_rotate.c
+++ instructions/r_rotate.c
@@ -6,7 +6,7 @@
 /*   By: amarti <amarti@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 10:45:50 by amarti            #+#    #+#             */
-/*   Updated: 2025/05/31 09:58:42 by amarti           ###   ########.fr       */
+/*   Updated: 2025/06/02 07:39:41 by amarti           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,34 +54,45 @@ void	rrb(t_node **stack_b)
 
 void	rrr(t_node **stack_a, t_node **stack_b)
 {
+	s_rra(stack_a);
+	s_rrb(stack_b);
+	write(1, "rrr\n", 4);
+}
+
+void	s_rrb(t_node **stack_b)
+{
 	t_node	*last;
 	t_node	*prev;
 
-	if (*stack_a && (*stack_a)->next)
+	if (!*stack_b || !(*stack_b)->next)
+		return ;
+	last = *stack_b;
+	prev = NULL;
+	while (last->next)
 	{
-		last = *stack_a;
-		prev = NULL;
-		while (last->next)
-		{
-			prev = last;
-			last = last->next;
-		}
-		prev->next = NULL;
-		last->next = *stack_a;
-		*stack_a = last;
+		prev = last;
+		last = last->next;
 	}
-	if (*stack_b && (*stack_b)->next)
+	prev->next = NULL;
+	last->next = *stack_b;
+	*stack_b = last;
+}
+
+void	s_rra(t_node **stack_a)
+{
+	t_node	*last;
+	t_node	*prev;
+
+	if (!*stack_a || !(*stack_a)->next)
+		return ;
+	last = *stack_a;
+	prev = NULL;
+	while (last->next)
 	{
-		last = *stack_b;
-		prev = NULL;
-		while (last->next)
-		{
-			prev = last;
-			last = last->next;
-		}
-		prev->next = NULL;
-		last->next = *stack_b;
-		*stack_b = last;
+		prev = last;
+		last = last->next;
 	}
-	write(1, "rrr\n", 4);
+	prev->next = NULL;
+	last->next = *stack_a;
+	*stack_a = last;
 }
